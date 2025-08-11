59eada57c5ed3a3b4251928442488f38202fb8c1
Karthikb2701@ <karthikb2701@gmail.com>
2025-08-11 18:55:16 +0530

    new file

---
+++ mongoC.js
@@ -0,0 +1,16 @@
+import { MongoClient } from "mongodb";
+
+//const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
+//const connectionString = `mongodb+srv://integrationninjas:${password}@devcluster.hu5pjmi.mongodb.net/?retryWrites=true&w=majority`; // cluster url
+const connectionString = `mongodb+srv://karthikb2701:Karthikb2701@cluster0.gcwvkms.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(connectionString);
+let conn;
+try {
+  conn = await client.connect();
+  console.log("connection successful");
+} catch (e) {
+  console.error(e);
+}
+let db = conn.db("integration_ninjas");
+export default db;
